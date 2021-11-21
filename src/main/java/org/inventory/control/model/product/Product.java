0e4acb828ed54aa92a3efd165fe0ce8e9eb410cb
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-21 16:36:54 -0300

    refactor: unique

--- src/main/java/org/inventory/control/model/product/Product.java
+++ src/main/java/org/inventory/control/model/product/Product.java
@@ -3,16 +3,18 @@ package org.inventory.control.model.product;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import org.inventory.core.database.PersistentEntity;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "Constraint_unique_code",
+        columnNames = {"code"}))
 public class Product extends PersistentEntity {
 
-    @Column(unique = true)
     private String code;
     private String name;
     private String description;
