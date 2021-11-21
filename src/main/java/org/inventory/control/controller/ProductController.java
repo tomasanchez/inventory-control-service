7fbfbf443923e557471b064daba66bfc3189452c
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2021-11-21 16:36:28 -0300

    refactor: return json

--- src/main/java/org/inventory/control/controller/ProductController.java
+++ src/main/java/org/inventory/control/controller/ProductController.java
@@ -1,13 +1,17 @@
 package org.inventory.control.controller;
 
+import java.util.Map;
+import com.google.gson.Gson;
 import org.inventory.control.model.product.Product;
 import org.inventory.control.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ProductController {
@@ -16,13 +20,19 @@ public class ProductController {
     private ProductRepository productRepository;
 
 
-    @PostMapping(path = "/products")
-    public @ResponseBody String onAddProduct(@RequestParam String name,
-            @RequestParam String description, @RequestParam String price) {
-        Product product = new Product().setName(name).setDescription(description)
-                .setPrice(Double.parseDouble(price));
+    @PostMapping(path = "/products", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody String onAddProduct(
+            @RequestParam(required = false) Map<String, String> allParams) {
+
+        Product product = new Product().setName(allParams.get("name"))
+                .setDescription(allParams.get("description"))
+                .setPrice(Double.parseDouble(allParams.get("price")))
+                .setCode(allParams.get("code"));
+
         productRepository.save(product);
-        return "Saved";
+
+        return new Gson().toJson(product);
     }
 
     @GetMapping(path = "/products")
